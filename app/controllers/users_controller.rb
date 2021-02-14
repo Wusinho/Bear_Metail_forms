29eae179eee5d2f65a9a57f1a94184425b1cd23e
Heber <heberlazo@gmail.com>
2021-02-13 19:28:03 -0500

    Add forms with html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
-    @user = User.new
-    if @user.save
-      redirect_to @User
-    else
-      render :new
-    end
+    # @user = User.new
+    # if @user.save
+    #   redirect_to @User
+    # else
+    #   render :new
+    # end
   end
 end
