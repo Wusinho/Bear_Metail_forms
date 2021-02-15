db5be162a2b379396e71381b0acacedc69aeef92
Heber <heberlazo@gmail.com>
2021-02-15 12:05:48 -0500

    Add form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -12,6 +13,21 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update
+      redirect_to root
+    else
+      render :edit
+    end
+  end
+
 
   private
 
